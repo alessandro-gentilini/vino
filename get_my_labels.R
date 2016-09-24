0d40332689d5acc4bf2e6aa2908148286068aaa6
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2016-09-24 17:45:44 +0200

    Remove row names (in order to make github happy).

--- get_my_labels.R
+++ get_my_labels.R
@@ -8,4 +8,4 @@ for(i in seq(1,nrow(df))){
   #download.file(as.character(df[i,]$Label.image),df[i,]$labels,"wget")
   #Sys.sleep(0.5)
 }
-write.table(df,"vini.csv",sep=",")
+write.table(df,"vini.csv",sep=",",row.names = FALSE)
