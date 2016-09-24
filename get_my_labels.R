d3c58308d0fd809b7cb7858690223498db5a8d52
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2016-09-24 17:28:52 +0200

    Add R script to download image of labels. Remove duplicate from vini.csv

---
+++ get_my_labels.R
@@ -0,0 +1,11 @@
+df <- read.csv("vini.csv",header=TRUE,sep=",")
+df$timestamp <- as.POSIXlt(df$Scan.date, "%Y-%m-%d %H:%M")
+df$timestamp <- strftime(df$timestamp , "%Y-%m-%dT%H%M")
+df$id<-seq(1,nrow(df))
+df$labels<-sprintf("%s_%s_%s_sd_%s.jpg",df$id,df$Wine.name,df$Vintage,df$timestamp)
+
+
+for(i in seq(1,nrow(df))){
+  download.file(as.character(df[i,]$Label.image),df[i,]$labels,"wget")
+  Sys.sleep(0.5)
+}
